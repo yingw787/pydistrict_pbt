82288ceb429eefc70cc1c3874746f2c0292918ab
Ying Wang <yingw787.personal@gmail.com>
2020-10-23 18:01:11 -0400

    Add password_validation.py test_password_validation.py.

---
+++ examples/password_validation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# This file defines a basic password validator, kind of like what you might see
+# in a production codebase.
+#
+# A password must be:
+# - At least 8 characters long
+# - Contains one special character (!@#$%^&*)
+# - Contains at least one uppercase letter
+# - Contains at least one lowercase letter
+# - Contains at least one number
---
+++ examples/test_password_validation.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+#
+# This file tests the password validator defined in 'password_validator.py'.
