db0287a541bece3af166a6c3f7e5744af8ae1e11
Ying Wang <yingw787.personal@gmail.com>
2020-10-23 18:19:40 -0400

    Update password_validation.py.

--- examples/password_validation.py
+++ examples/password_validation.py
@@ -9,3 +9,28 @@
 # - Contains at least one uppercase letter
 # - Contains at least one lowercase letter
 # - Contains at least one number
+
+import string
+
+
+def password_is_valid(password):
+    """
+    @param password
+    @return: password is valid
+    """
+    if len(password) < 8:
+        return False
+
+    if not any([c in password for c in "!@#$%^&*"]):
+        return False
+
+    if not any([c in password for c in string.ascii_lowercase]):
+        return False
+
+    if not any([c in password for c in string.ascii_uppercase]):
+        return False
+
+    if not any([c in password for c in string.digits]):
+        return False
+
+    return True
