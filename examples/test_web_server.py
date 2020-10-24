301bb9412154a0c24b858ddb58f109987f6f71ed
Ying Wang <yingw787.personal@gmail.com>
2020-10-24 12:02:50 -0400

    Add web_server.py test_web_server.py.

---
+++ examples/test_web_server.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+#
+# This file tests the basic web server defined in 'web_server.py'.
---
+++ examples/web_server.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+#
+# This file defines a basic web server, what you might find at a small company
+# or internal tools team.
