70b025e5858d94b7d78d8ea6da8355cb3c447f94
Ying Wang <yingw787.personal@gmail.com>
2020-10-27 15:50:40 -0400

    Update test_web_server.py.

--- examples/test_web_server.py
+++ examples/test_web_server.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 #
 # This file tests the basic web server defined in 'web_server.py'.
+
+import pytest
+
+from web_server import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as testing_client:
+        with app.app_context():
+            yield testing_client
+
+
+def test_init_returns_ok(client):
+    response = client.get("/init")
+    assert response.status_code == 200
+
+
+def test_test_returns_ok_with_params(client):
+    response = client.get("/test?name=Ted&age=25")
+    assert response.status_code == 200
