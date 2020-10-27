8088e4ff4935564710d2cc6fe9ed4bfd627ade11
Ying Wang <yingw787.personal@gmail.com>
2020-10-27 12:46:03 -0400

    Update test_password_validation.py.

--- examples/test_password_validation.py
+++ examples/test_password_validation.py
@@ -7,6 +7,17 @@ import pytest
 from password_validation import password_is_valid
 
 
-@pytest.mark.parametrize("_input, _expected", [("", False)])
+@pytest.mark.parametrize(
+    "_input, _expected",
+    [
+        ("", False),
+        ("ohautoeauna", False),
+        ("732814084212", False),
+        ("OAHUETNASU", False),
+        ("&*&*()&", False),
+        ("789&(*&(ht", False),
+        ("aBcD1234@", True),
+    ],
+)
 def test_password_matches_validation_for_inputs(_input, _expected):
     assert password_is_valid(_input) == _expected
