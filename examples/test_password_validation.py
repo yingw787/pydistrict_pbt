b0c2998bb82abe6be3ff8e64eddc81762a881121
Ying Wang <yingw787.personal@gmail.com>
2020-10-27 14:39:32 -0400

    Update test_password_validation.py working hypothesis validation here.

--- examples/test_password_validation.py
+++ examples/test_password_validation.py
@@ -2,11 +2,13 @@
 #
 # This file tests the password validator defined in 'password_validator.py'.
 
+from hypothesis import given, strategies as st
 import pytest
 
 from password_validation import password_is_valid
 
 
+# Regular unit test suite
 @pytest.mark.parametrize(
     "_input, _expected",
     [
@@ -21,3 +23,9 @@ from password_validation import password_is_valid
 )
 def test_password_matches_validation_for_inputs(_input, _expected):
     assert password_is_valid(_input) == _expected
+
+
+# `hypothesis` test suite
+@given(st.from_regex(r"(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]){8,}"))
+def test_password_matches_validation_for_arbitrary_inputs(_input):
+    assert password_is_valid(_input)
