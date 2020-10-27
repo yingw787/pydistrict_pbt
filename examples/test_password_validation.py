295b894f22d3dee088245a8e1dc088d50bfc840e
Ying Wang <yingw787.personal@gmail.com>
2020-10-27 12:43:44 -0400

    Update test_password_validation.py.

--- examples/test_password_validation.py
+++ examples/test_password_validation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 #
 # This file tests the password validator defined in 'password_validator.py'.
+
+import pytest
+
+from password_validation import password_is_valid
+
+
+@pytest.mark.parametrize("_input, _expected", [("", False)])
+def test_password_matches_validation_for_inputs(_input, _expected):
+    assert password_is_valid(_input) == _expected
